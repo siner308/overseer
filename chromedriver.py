be338f01dc4f50529b9f94a2ecfe26bc1e8ccb05
siner308 <siner308@gmail.com>
2020-11-20 00:02:38 +0900

    initial commit

---
+++ chromedriver.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from config import CHROME_DRIVER_PATH
+
+
+def setup_chrome() -> WebDriver:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(CHROME_DRIVER_PATH, chrome_options=chrome_options)
+    return driver
